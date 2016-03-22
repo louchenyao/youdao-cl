c67d61641231a3e6f2574443dc5bda4e57b71271
Chenyao2333 <841144792@qq.com>
2016-03-22 16:40:29 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,36 @@
+# Youdao Comandline tools
+
+The youdao-cl is a script for translate words and sentence, translating by Youdao.
+
+It's without any third-party packages dependency and able to run under either Python 2 or Python 3.
+
+# Usage 
+
+~~~
+python yd.py word
+~~~
+
+Tips: You can add yd.py directory into your PATH environment variable. Then you can run youdao-cl like `yd.py word` at anywhere.
+
+# Example
+
+~~~
+louch@louch-pc:~/projects/youdao_cl$ python yd.py trump
+Basic:
+    uk-phonetic: trʌmp
+    us-phonetic: trʌmp
+    phonetic: trʌmp
+    1. n. 王牌；法宝；喇叭
+    2. vt. 胜过；打出王牌赢
+    3. vi. 出王牌；吹喇叭
+    4. n. (Trump)人名；(英)特朗普
+Translation:
+    特朗普
+Web:
+    TRUMP:
+        特朗普; 键击噪声; 王牌; 
+    Trump Organization:
+        特朗普公司; 特朗普集团; 川普集团; 
+    Trump Plaza:
+        特朗普广场; 泉在大西洋城黄牌赌场; 川普大厦; 
+~~~
---
+++ yd.py
@@ -0,0 +1,85 @@
+#! /usr/bin/env python
+# -*- coidng: utf-8 -*-
+
+from __future__ import print_function
+
+import sys
+
+if sys.version_info[0] == 2:
+    PY2 = True
+else:
+    PY2 = False
+
+if PY2:
+    import httplib as hc
+    from urllib import urlencode
+else:
+    import http.client as hc
+    from urllib.parse import urlencode
+
+import argparse
+import json
+
+def query(word):
+    params = {
+        "keyfrom": "wordstash",
+        "key": "908579755",
+        "type": "data",
+        "doctype": "json",
+        "version": "1.1",
+        "q": word,
+    }
+    
+    url = "/openapi.do?" + urlencode(params)
+    conn = hc.HTTPConnection("fanyi.youdao.com")
+    conn.request("GET", url)
+    rep = conn.getresponse()
+    data = rep.read()
+    conn.close()
+
+    data = data.decode("utf-8")
+    data = json.loads(data)
+    return data
+
+def pretty_print(res):
+    if res["errorCode"]:
+        print("erroCode = %d\n" % res["errorcode"])
+        return
+
+    if "basic" in res:
+        print("Basic:")
+        basic = res["basic"]
+        if "uk-phonetic" in basic: print(u"    uk-phonetic: %s" % basic["uk-phonetic"]);
+        if "us-phonetic" in basic: print(u"    us-phonetic: %s" % basic["us-phonetic"]);
+        if "phonetic" in basic:    print(u"    phonetic: %s" % basic["phonetic"]);
+        if "explains" in basic:
+            cnt = 1;
+            for explain in basic["explains"]:
+                print(u"    %s. " % cnt + explain)
+                cnt += 1
+
+    if "translation" in res:
+        print("Translation:")
+        for line in res["translation"]:
+            print(u"    " + line);
+
+    if "web" in res:
+        print("Web:")
+        for item in res["web"]:
+            print(u"    %s:" % item["key"])
+            print(u"        ", end=u"")
+            for trans in item["value"]:
+                print("%s; " % trans, end=u"")
+            print()
+
+def main(word):
+    res = query(word)
+    pretty_print(res)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("word")
+    args = parser.parse_args()
+
+    word = args.word
+    main(word)
